7d253361c7d3cb1befe0c346dce8b5bd3be18131
Fam Shihata <fam@awadlouis.com>
2026-04-13 15:53:11 +0200

    feat: add control gateway controller

---
+++ src/control/control_gateway/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'control_gateway'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Fam Shihata',
+    maintainer_email='fam@awadlouis.com',
+    description='TODO: Package description',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "control_gateway = your_package.control_gateway:main",
+        ],
+    },
+)
