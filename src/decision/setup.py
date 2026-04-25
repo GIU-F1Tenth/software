5a28fe120fdfc4a345fd3d030e88a865ea10954e
FamALouiz <fam@awadlouis.com>
2026-04-25 16:55:52 +0200

    feat: update setup.py for new node

--- src/decision/setup.py
+++ src/decision/setup.py
@@ -1,29 +1,31 @@
 from setuptools import find_packages, setup
 
-package_name = 'decision'
+package_name = "decision"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/config", ["config/fsm_params.yaml"]),
+        ("share/" + package_name + "/launch", ["launch/fsm_node.launch.py"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Fam Shihata',
-    maintainer_email='fam@awadlouis.com',
-    description='TODO: Package description',
-    license='MIT',
+    maintainer="Fam Shihata",
+    maintainer_email="fam@awadlouis.com",
+    description="Decision making module",
+    license="MIT",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "fsm_node=decision.fsm.fsm_node:main",
         ],
     },
 )
